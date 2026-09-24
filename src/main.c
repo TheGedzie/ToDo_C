735af6f33693b7b005849d6a7b119dd3b9f7ea52
TheGedzie <gedzie06@gmail.com>
2026-09-24 23:52:42 +0700

    Add function addTask, printList and new funcional in main

--- src/main.c
+++ src/main.c
@@ -1,30 +1,32 @@
 #include "../include/task.h"
 
+void printList(const List *list); 
+void addTask (List *list);
 
 int main() {
   List my_list = {
       .all_elements = 4,
       .tasks = {
         {
-          .ID = 101,
+          .ID = 100,
           .TaskName = "Выучить основы структур в Си",
-          .Time = 120,
+          .Time = 119,
+          .isComplete = true
+        },
+        {
+          .ID = 101,
+          .TaskName = "Настроить Makefile и .gitignore",
+          .Time = 44,
           .isComplete = true
         },
         {
           .ID = 102,
-          .TaskName = "Настроить Makefile и .gitignore",
-          .Time = 45,
-          .isComplete = true
-        },
-        {
-          .ID = 103,
           .TaskName = "Написать функцию вывода списка на экран",
-          .Time = 30,
+          .Time = 29,
           .isComplete = false
         },
         {
-          .ID = 104,
+          .ID = 103,
           .TaskName = "Помыть посуду",
           .Time = 15,
           .isComplete = false
@@ -32,14 +34,72 @@ int main() {
       }
   };
 
-  printf("=== СПИСОК ЗАДАЧ ===\n");
-  printf("Всего задач созданно: %d\n", my_list.all_elements);
-  for(int i = 0; i < my_list.all_elements; i++){
-    printf("[%s] ID: %d\t | %s \t\t | Время: %d \n", 
-           my_list.tasks[i].isComplete ? "X" : " ",
-           my_list.tasks[i].ID,
-           my_list.tasks[i].TaskName,
-           my_list.tasks[i].Time);
-  }
-    return 0;
+  int variant;
+
+  do {
+    printf("\tВЕДИТЕ\n 1 - получить список задач\n 2 - добавить задачу\n 0 - выйти\n--> ");
+    scanf("%d", &variant); 
+  
+    switch (variant) {
+      case 1:
+        printList(&my_list);
+        break;  
+      case 2:
+        addTask(&my_list);
+        break;
+      case 0: 
+        break; 
+    }
+
+  } while (variant != 0);
+
+  return 0;
 }
+
+void printList(const List *list){
+  printf("=== СПИСОК ЗАДАЧ ===\n");
+  printf("Всего задач созданно: %d\n", list->all_elements);
+  for(int i = 0; i < list->all_elements; i++){
+    printf("[%-5s] ID: %-10d\t | %-10s \t | Время: %-10d \n", 
+           list->tasks[i].isComplete ? "X" : " ",
+           list->tasks[i].ID,
+           list->tasks[i].TaskName,
+           list->tasks[i].Time);
+  }
+}
+
+void addTask (List *list){
+  
+  // создаем указатель на новую таску
+  Task_S *new_task = &list->tasks[list->all_elements];  
+
+  // Проверка на максимальное количество тасок 
+  if (list->all_elements > MAX_ELEMENTS) {
+    printf("Создано максиальное количество задач !");
+    return; 
+  }
+
+  // получаем динамически id: берем id последней таски и доавляем к нему 1
+  // если это первый элемент, то ему автоматически ставться id 100
+  if (list->all_elements == 0) {
+    new_task->ID = 100; 
+  }
+  else {
+  new_task->ID = list->tasks[list->all_elements - 1].ID + 1;
+  }
+
+  //  Добавляем то что задача не выполнена
+  new_task->isComplete = false; 
+
+  // Пользователь вводит имя задачи и мы добавляем его в новую таску 
+  printf("Название задачи: ");
+  scanf(" %[^\n]", &new_task->TaskName);
+  
+  // Пользователь вводит крайний срок задачи и мы добавляем его в новую таску 
+  printf("Время задачи: ");
+  scanf("%d", &new_task->Time);
+  
+  // Увеличиваем счетчик элементов 
+  list->all_elements++; 
+}
+
